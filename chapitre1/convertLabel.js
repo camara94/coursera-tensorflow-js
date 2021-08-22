3d3603339a56f6ddd27220d7b4f9be3c00fbed32
Laby Damaro Camara <ldamaro98@gmail.com>
2021-08-22 05:44:14 +0200

    dataset

--- chapitre1/convertLabel.js
+++
@@ -1,9 +0,0 @@
-const convertLabel = trainingData.map( ( { xs, ys } ) => {
-    const labels = [
-        ys.species === 'setosa' ? 1 : 0,
-        ys.species === 'virginica' ? 1 : 0,
-        ys.species === 'versicolor' ? 1 : 0
-    ];
-
-    return  { xs: Object.values( xs ), ys: Object.values(labels) }
-} ).batch( 10 );
